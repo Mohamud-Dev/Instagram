e32840f37a1129421de463e38100da61dfbd8820
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-18 01:07:32 +0300

    import login_required

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
+@login_required(login_url='/accounts/login/')
+
+def register(request):
+    return render(request,'django_registration/registration_form.html')
