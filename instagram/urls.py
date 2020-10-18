2b9e1481031b1df9705aa6ea7686d1a47a9c7ea5
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-18 20:34:05 +0300

    Add a reguster url

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    # path('logout/', views.logout_view,{"next_page":'/'}),
 ]
