93957bc7bb622bbec8220b4b2d24673652f2d5db
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-20 17:53:54 +0300

    Add a profile url

--- insta/urls.py
+++ insta/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 from django.conf import settings
-# from django.conf.urls.static import static
+from insta import views as user_views
+
+
 
 
 urlpatterns=[
     path('',views.home,name = 'home'),
     path('accounts/register/', views.register, name='register'),
-    
-
+    path('new_post/', views.new_post,name ='new_post'),
+    path('profile/', user_views.profile,name = 'profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
