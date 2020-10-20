a00d7a407c4c38d5b5a0875e4676dfd9dda2ea25
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-20 17:56:19 +0300

    IMport tinmyce

--- insta/models.py
+++ insta/models.py
@@ -6,9 +6,10 @@ from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
-    name =  models.CharField(max_length = 30)
-    profile_pic = models.ImageField(upload_to = 'images/',default = '')
+    name = models.CharField(max_length=30)
+    profile_pic = models.ImageField(upload_to='new_post/', blank = 'true')
     bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='', null=True)
 
     def save_profile(self):
         self.save
@@ -16,16 +17,25 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
+    # def __str__(self):
+    #     return self.name
     def __str__(self):
-        return self.name
-
-    
-class  Post(models.Model):
-    image = models.ImageField(blank=True)
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
-    name = models.CharField(max_length=30)
-    post = HTMLField()
-    caption = models.CharField(max_length=300) 
-    post = models.TextField()
+        return f'{self.user.username} Profile'
 
 
+class Post(models.Model):
+    image = models.ImageField(upload_to='new_post/', blank=True ,default = 'default.jpg')
+    title = models.CharField(max_length=30, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='', null=True)
+    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='')
+    caption = models.TextField(max_length=300)
+    # post = HTMLField()
+    # likes = manyToMany
+
+    @classmethod
+    def all_posts(cls) :
+        posts = cls.objects.all()
+        return posts
+
+    def __str__(self):
+        return self.title
