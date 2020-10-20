3de4ffb6f006d2b832ab0feb1e7c80fac4a02c79
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-20 17:49:19 +0300

    Add a profile picture and redirect it to the home .html

--- insta/views.py
+++ insta/views.py
@@ -1,15 +1,58 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm
+from .forms import SignUpForm, NewPostForm
 from django.contrib.auth.models import User
+from .forms import Post, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.all_posts()
+    json_posts = []
+    for post in posts:
+
+        # import pdb; pdb.set_trace()
+        pic = Profile.objects.filter(user=post.user.id).first()
+        pic = pic.profile_pic.url
+        obj = dict(
+            image=post.image.url,
+            author=post.user.username,
+            avatar=pic,
+            name=post.title,
+            caption=post.caption
+
+        )
+        json_posts.append(obj)
+    return render(request, 'home.html', {"posts": json_posts})
+
+
+def profile(request):
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+
+        }
+
+    return render(request, 'profile.html', context)
 
 
 def register(request):
@@ -26,8 +69,20 @@ def register(request):
         form = SignUpForm()
     return render(request, 'registration/registration_form.html', {'form': form})
 
-# def login(request):
-#     return render(request,'home.html')
-# def logout_view(request):
-#     logout(request)
-#     return render(request, 'home.html')
+
+def new_post(request):
+    current_user = request.user
+    # current_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            # image.profile = current_profile
+            image.save()
+            # print(current_profile)
+        return redirect('home')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
